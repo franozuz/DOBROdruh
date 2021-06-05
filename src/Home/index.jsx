7f7bed02624c0f619a27b807ae55e3e17b6e96c4
Zuzana Franova <franova.z@gmail.com>
2021-06-05 11:14:59 +0200

    home text

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Home = () => {
-  return <h2>Domov</h2>;
+  return (
+    <>
+    <h1>Jsi pravý DOBROdruh?</h1>
+      <p>
+        Cestování je skvělé, rozširuje naše obzory a pomáhá nám objevovat nové
+        ůhly pohledu. Cestování může mít ale i temné stránky, a to negativní
+        vliv na životní prostředí. Toitiž, ne každý cestovatel je DOBROdruh.
+        Někteří jsou spíš ZLOdruhové. ZLOdruhové hážou na zem plastové láhve,
+        ignorují zákazy vstupu v chráněných oblatech, a tak dále. Jak na tom
+        jseš ty? Jsi vzorný DOBROdruh?
+      </p>
+      <h3>Otestuj se v našem testu a zjisti, co ještě můžeš zlepšit.</h3>
+      <button>Spustit test</button>
+    </>
+  );
 }
 
 export default Home;
