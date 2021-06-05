495584f7c0696fd774aa56fed9625a735c569f75
Zuzana Franova <franova.z@gmail.com>
2021-06-05 20:14:09 +0200

    projects img add

--- src/Projects/index.jsx
+++ src/Projects/index.jsx
@@ -7,6 +7,15 @@ const Projects = () => {
       <h2>Podpoř vybraný projekt a zneutralizuj svou uhlíkovou stopu!</h2>
       <div className="projectList">
         <article className="project">
+          <img src="/assets/img/hnuti-duha.jpg" alt="Hnuti Duha" />
+          <h3>Zasaď strom </h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+            ducimus repellat est delectus distinctio quisquam maxime
+          </p>
+        </article>
+        <article className="project">
+          <img src="/assets/img/hnuti-duha.jpg" alt="Hnuti Duha" />
           <h3>Zasaď strom </h3>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
@@ -55,14 +64,6 @@ const Projects = () => {
             ducimus repellat est delectus distinctio quisquam maxime
           </p>
         </article>
-        <article className="project">
-          <h3>Zasaď strom </h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
-            ducimus repellat est delectus distinctio quisquam maxime
-          </p>
-        </article>
-        
       </div>
     </>
   );
