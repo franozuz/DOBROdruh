d5cf5e5fce5779167e07bf13336894f70756d52f
Zuzana Franova <franova.z@gmail.com>
2021-06-05 19:32:27 +0200

    calculate result and display it

--- src/Test/Result/index.jsx
+++ src/Test/Result/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './style.css';
 
-const Result = () => {
+const Result = (props) => {
   return (
-    <>
+    <div className="result">
       <h1>SI ZLODRUH!!!!!!!</h1>
-    </>
+      <h2>{props.result}</h2>
+    </div>
   );
 };
 
