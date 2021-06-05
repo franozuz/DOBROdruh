abb0d23689ba656198ed977066cf391dc4f13dbe
Zuzana Franova <franova.z@gmail.com>
2021-06-05 09:48:02 +0200

    React Router

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,79 @@
 import React from 'react';
 import { render } from 'react-dom';
 import './style.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-const App = () => (
-  <div className="container">
-    <header>
-      <div className="logo"></div>
-      <h1>Webová aplikace</h1>
-    </header>
-    <main>
-      <p>Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>.</p>
-    </main>
-    <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </footer>
-  </div>
-);
+const Home = () => {
+  return <h2>Domov</h2>;
+}
+
+const Test = () => {
+  return <h2>Test</h2>;
+}
+
+const Calculator = () => {
+  return <h2>CO2 Kalkulačka</h2>;
+}
+
+const Tips = () => {
+  return <h2>Cestovatelské tipy</h2>;
+}
+
+const Projects = () => {
+  return <h2>Eko projekty</h2>;
+}
+
+
+
+const App = () => {
+  return (
+    <Router>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Domov</Link>
+          </li>
+          <li>
+            <Link to="/test">Test</Link>
+          </li>
+          <li>
+            <Link to="/calculator">CO2 Kalkulačka</Link>
+          </li>
+          <li>
+            <Link to="/tips">Cestovatelské tipy</Link>
+          </li>
+          <li>
+            <Link to="/projects">EKO projekty</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/test">
+          <Test />
+        </Route>
+        <Route path="/calculator">
+          <Calculator />
+        </Route>
+        <Route path="/tips">
+          <Tips />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
+  )
+};
 
 render(<App />, document.querySelector('#app'));
