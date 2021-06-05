e9b6c4194e97373b18fbaf9c5b38e3bbdc9edc17
Zuzana Franova <franova.z@gmail.com>
2021-06-05 12:34:28 +0200

    Test background image added

--- src/Test/Question/index.jsx
+++ src/Test/Question/index.jsx
@@ -4,18 +4,19 @@ import './style.css';
 
 const Question = (props) => {
   return (
-    <>
+    <div className="question">
+    
     <h2>{props.question.question}</h2>
     
     <form>
     {props.question.answers.map((answer) => 
     <label key={answer.id}> 
-    <input type="radio" name="question" value={answer.value}/>
+    <input type="radio" name="answer" value={answer.value}/>
     {answer.text}
     </label>
     )}
     </form>
-    </>
+    </div>
   )
 };
 
