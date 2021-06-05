b412dc21454d232d96ac929793f131d70c4b31ed
Julia Micekova <julka_micekova@yahoo.co.uk>
2021-06-05 13:48:31 +0200

    solved global style conflict

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,40 +1,45 @@
 import React from 'react';
+import './style.css';
 
 const Home = () => {
   return (
     <>
-      <section className="test">
+      <section className="home-test">
         <img
-          className="illustration"
           src="/assets/img/Illus_MEDIUM.png"
           alt="illustration woman and man trawellers"
         />
-        <h1>Jsi pravý DOBROdruh?</h1>
-        <p>
-          Cestování je skvělé, rozširuje naše obzory a pomáhá nám objevovat nové
-          ůhly pohledu. Cestování může mít ale i temné stránky, a to negativní
-          vliv na životní prostředí. Toitiž, ne každý cestovatel je DOBROdruh.
-          Někteří jsou spíš ZLOdruhové. ZLOdruhové hážou na zem plastové láhve,
-          ignorují zákazy vstupu v chráněných oblatech, a tak dále. Jak na tom
-          jseš ty? Jsi vzorný DOBROdruh?
-        </p>
-        <h3>Otestuj se v našem testu a zjisti, co ještě můžeš zlepšit.</h3>
-        <button>Spustit test</button>
+        <article>
+          <h1>Jsi pravý DOBROdruh?</h1>
+          <p>
+            Cestování je skvělé, rozširuje naše obzory a pomáhá nám objevovat
+            nové ůhly pohledu. Cestování může mít ale i temné stránky, a to
+            negativní vliv na životní prostředí. Toitiž, ne každý cestovatel je
+            DOBROdruh. Někteří jsou spíš ZLOdruhové. ZLOdruhové hážou na zem
+            plastové láhve, ignorují zákazy vstupu v chráněných oblatech, a tak
+            dále. Jak na tom jseš ty? Jsi vzorný DOBROdruh?
+          </p>
+          <h3>Otestuj se v našem testu a zjisti, co ještě můžeš zlepšit.</h3>
+          <button className="btn-test">Spustit test</button>
+        </article>
       </section>
       <section className="calculator">
-        <h2>Zjisti a vykompenzuj uhlíkovou stopu své příští cesty</h2>
-        <p>
-          Turizmus je odpovědný za přibližně 8% celosvětových uhlíkových emisí.
-          Zadej místo, odkud cestuješ, cílovou destinaci a způsob dopravy.
-        </p>
-        <form action="">
-          <label htmlFor="">
-            <input type="Odkud" placeholder="Odkud" />
-            <input type="Kam" placeholder="Kam" />
-            <input type="Typ dopravy" placeholder="Typ dopravy" />
-          </label>
-        </form>
-        <button>Spočítat</button>
+        <article>
+          <h2>Zjisti a vykompenzuj uhlíkovou stopu své příští cesty</h2>
+          <p>
+            Turizmus je odpovědný za přibližně 8% celosvětových uhlíkových
+            emisí. Zadej místo, odkud cestuješ, cílovou destinaci a způsob
+            dopravy.
+          </p>
+          <form action="">
+            <label htmlFor="">
+              <input type="Odkud" placeholder="Odkud" />
+              <input type="Kam" placeholder="Kam" />
+              <input type="Typ dopravy" placeholder="Typ dopravy" />
+            </label>
+          </form>
+          <button className="btn-test">Spočítat</button>
+        </article>
       </section>
       <section className="projects">
         <h2>Podpor vybraný projekt a zneutralizuj svou uhlíkovou stopu!</h2>
--- src/Test/index.jsx
+++ src/Test/index.jsx
@@ -1,19 +1,16 @@
-import React from "react";
-import Question from "./Question";
-import questions from "./questions.json"
+import React from 'react';
+import Question from './Question';
+import questions from './questions.json';
 import './style.css';
 
-
 const Test = () => {
-  const question = questions[0]
+  const question = questions[0];
 
   return (
-    <section>
+    <section className="test">
       <Question question={question} />
     </section>
-  
   );
-}
-
+};
 
 export default Test;
