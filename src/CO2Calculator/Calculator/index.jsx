952499d583b2eed5a14ec9f63062ae0c2a9759d6
Julia Micekova <julka_micekova@yahoo.co.uk>
2021-06-06 13:54:39 +0200

    round two decimals results carbonfootprint car

--- src/CO2Calculator/Calculator/index.jsx
+++ src/CO2Calculator/Calculator/index.jsx
@@ -1,14 +1,34 @@
 import React, { useState } from 'react';
 import './style.css';
 
+const roundTwo = (number) => {
+  return Math.round(number * 100) / 100;
+};
+
+const calculateCarFuel = (distance, consumption) => {
+  return roundTwo((consumption / 100) * distance * 2.39);
+};
+
+const calculateCarDiesel = (distance, consumption) => {
+  return roundTwo((consumption / 100) * distance * 2.62);
+};
+
 const Calculator = () => {
   const [distance, setDistance] = useState(0);
   const [consumption, setConsumption] = useState(0);
   const [fuel, setFuel] = useState('gas');
+  const [calculationResult, setCalculationResult] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(distance, consumption, fuel);
+    if (fuel === 'gas') {
+      const result = calculateCarFuel(distance, consumption);
+      setCalculationResult(result);
+    }
+    if (fuel === 'diesel') {
+      const result = calculateCarDiesel(distance, consumption);
+      setCalculationResult(result);
+    }
   };
 
   return (
@@ -30,9 +50,9 @@ const Calculator = () => {
         onChange={(e) => setConsumption(Number(e.target.value))}
         type="number"
         min="0"
-        placeholder="Spotřeba (l/km)"
+        placeholder="Spotřeba (l/100km)"
       />
-      <select onChange={(e) => setFuel(Number(e.target.value))} name="fuel">
+      <select onChange={(e) => setFuel(e.target.value)} name="fuel">
         <option value="gas">Benzín</option>
         <option value="diesel">Diesel</option>
       </select>
@@ -40,6 +60,7 @@ const Calculator = () => {
       <button className="btn-calculator" type="submit">
         Spočítat
       </button>
+      {calculationResult}
     </form>
   );
 };
