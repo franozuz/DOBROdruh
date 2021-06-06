0b0e9ba815665a64ab1749627b83e62a17c1b317
Zuzana Franova <franova.z@gmail.com>
2021-06-06 18:54:35 +0200

    typo on home page corrected

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -18,7 +18,7 @@ const Home = () => {
             nové ůhly pohledu. Cestování může mít ale i temné stránky, a to
             negativní vliv na životní prostředí. Toitiž, ne každý cestovatel je
             DOBROdruh. Někteří jsou spíš ZLOdruhové. ZLOdruhové hážou na zem
-            plastové láhve, ignorují zákazy vstupu v chráněných oblatech, a tak
+            plastové láhve, ignorují zákazy vstupu v chráněných oblastech, a tak
             dále. Jak na tom jseš ty? Jsi vzorný DOBROdruh?
           </p>
           <h3>Otestuj se v našem testu a zjisti, co ještě můžeš zlepšit.</h3>
