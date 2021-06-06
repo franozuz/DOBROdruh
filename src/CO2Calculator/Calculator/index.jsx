99bfa69f543f1cc201495539babebf8620b9a929
Julia Micekova <julka_micekova@yahoo.co.uk>
2021-06-06 12:21:14 +0200

    add functionality to calculation form

--- src/CO2Calculator/Calculator/index.jsx
+++ src/CO2Calculator/Calculator/index.jsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 const Calculator = () => {
+  const [distance, setDistance] = useState(0);
+  const [consumption, setConsumption] = useState(0);
+  const [fuel, setFuel] = useState('gas');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(distance, consumption, fuel);
+  };
+
   return (
-    <form className="calculator-form">
-      <select name="druh dopravy">
-        <option value="plane">Letadlo</option>
+    <form onSubmit={handleSubmit} className="calculator-form">
+      <select name="transport">
         <option value="car">Auto</option>
+        {/* <option value="plane">Letadlo</option>
         <option value="train">Train</option>
         <option value="bike">Kolo</option>
-        <option value="byfoot">Pěšky</option>
+        <option value="byfoot">Pěšky</option> */}
       </select>
-      <input type="number" placeholder="Vzdálenost (km)" />
-      {/* <input type="text" placeholder="Kam" />
-       */}
+      <input
+        onChange={(e) => setDistance(Number(e.target.value))}
+        type="number"
+        min="0"
+        placeholder="Vzdálenost (km)"
+      />
+      <input
+        onChange={(e) => setConsumption(Number(e.target.value))}
+        type="number"
+        min="0"
+        placeholder="Spotřeba (l/km)"
+      />
+      <select onChange={(e) => setFuel(Number(e.target.value))} name="fuel">
+        <option value="gas">Benzín</option>
+        <option value="diesel">Diesel</option>
+      </select>
+
       <button className="btn-calculator" type="submit">
         Spočítat
       </button>
