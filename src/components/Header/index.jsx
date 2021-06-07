189dc4311178ae8a5452d9f460be2b718a111bc0
Julia Micekova <julka_micekova@yahoo.co.uk>
2021-06-08 01:14:56 +0200

    added Link for Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './style.css';
 
 const Header = () => {
@@ -24,29 +25,43 @@ const Header = () => {
           <img src="/assets/img/logo_DOBROdruh.svg" alt="logo DOBROdruh" />
           <ul>
             <li>
-              <a onClick={handleClose} href="/" className="nav-link">
-                Domov
-              </a>
+              <Link to="/">
+                <a onClick={handleClose} href="/" className="nav-link">
+                  Domov
+                </a>
+              </Link>
             </li>
             <li>
-              <a onClick={handleClose} href="/test/1" className="nav-link">
-                Test
-              </a>
+              <Link to="/test/1">
+                <a onClick={handleClose} href="/test/1" className="nav-link">
+                  Test
+                </a>
+              </Link>
             </li>
             <li>
-              <a onClick={handleClose} href="/calculator" className="nav-link">
-                CO2 Kalkulačka
-              </a>
+              <Link to="/calculator">
+                <a
+                  onClick={handleClose}
+                  href="/calculator"
+                  className="nav-link"
+                >
+                  CO2 Kalkulačka
+                </a>
+              </Link>
             </li>
             <li>
-              <a onClick={handleClose} href="/tips" className="nav-link">
-                Cestovatelské tipy
-              </a>
+              <Link to="/tips">
+                <a onClick={handleClose} href="/tips" className="nav-link">
+                  Cestovatelské tipy
+                </a>
+              </Link>
             </li>
             <li>
-              <a onClick={handleClose} href="/projects" className="nav-link">
-                EKO projekty
-              </a>
+              <Link to="/projects">
+                <a onClick={handleClose} href="/projects" className="nav-link">
+                  EKO projekty
+                </a>
+              </Link>
             </li>
           </ul>
         </nav>
